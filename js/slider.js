88f85d2ed55a3f19e94d3398df32664e49a8dd88
carlos-osorio-developer <osorio.uis@gmail.com>
2021-03-07 19:41:35 -0500

    Add sections Pedagogy and Contact us, fully responsive

---
+++ js/slider.js
@@ -0,0 +1,44 @@
+(function(){
+
+//Propiedades Slider
+
+var propSlider = {
+
+	slider : document.getElementById('slider'),
+	primerSlide : null
+
+}
+
+
+
+//Métodos Slider
+
+var metSlider = {
+
+	inicio : function() {
+
+		setInterval(metSlider.moverSlide, 3000);
+	},
+
+	moverSlide : function () {
+		propSlider.slider.style.transition = 'all 1s ease';
+		propSlider.slider.style.marginLeft = '-70%'; 
+		propSlider.slider.style.marginTop = '-70%';
+
+		setTimeout(function(){
+			propSlider.primerSlide = propSlider.slider.firstElementChild;
+			propSlider.slider.appendChild(propSlider.primerSlide);
+
+			propSlider.slider.style.transition = 'unset';
+
+			propSlider.slider.style.marginLeft = '0';
+			propSlider.slider.style.marginTop = '0';
+		}, 1000);
+	}
+
+
+}
+
+metSlider.inicio();
+
+}())
